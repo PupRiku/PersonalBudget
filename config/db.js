67e5ed3dfe977aa300931811e64b77de6d2aff2e
Chris Diorio <11452673+PupRiku@users.noreply.github.com>
2021-02-01 09:22:29 -0600

    database configs

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const envelopes = [
+    {
+        id: 1,
+        title: "Groceries",
+        budget: 300
+    },
+    {
+        id: 2,
+        title: "Gaming",
+        budget: 100
+    },
+    {
+        id: 3,
+        title: "Dining Out",
+        budget: 200
+    },
+];
+
+module.exports = envelopes;
