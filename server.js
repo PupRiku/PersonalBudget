609e6e157b4dbfb54800245e39a76b1e985caba8
Chris Diorio <11452673+PupRiku@users.noreply.github.com>
2021-02-01 17:48:26 -0600

    setup server

--- server.js
+++ server.js
@@ -4,16 +4,19 @@ const logger = require("morgan");
 
 dotenv.config({ path: "./config/config.env" });
 
+const envelopesRouter = require("./routes/envelopes");
+const docsRouter = require("./routes/docs");
+
 const app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+app.use("/api-docs", docsRouter);
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+app.use("/api/v1/envelopes", envelopesRouter);
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
