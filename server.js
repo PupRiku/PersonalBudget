e5189bc9f5ccfc042482b5464b168749029fade1
Chris Diorio <11452673+PupRiku@users.noreply.github.com>
2021-02-01 09:29:41 -0600

    update server code

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const dotenv = require("dotenv");
+const logger = require("morgan");
+
+dotenv.config({ path: "./config/config.env" });
+
 const app = express();
-const PORT = 3000;
+
+app.use(logger("dev"));
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
 app.listen(PORT, () => {
-    console.log(`Example app listening at http://localhost:${PORT}`);
+    console.log(`Listening on port ${PORT}`);
 });
