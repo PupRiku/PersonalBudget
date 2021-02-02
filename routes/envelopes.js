3dd3a6a31b5285c779f24126c851bd27041746e5
Chris Diorio <11452673+PupRiku@users.noreply.github.com>
2021-02-01 19:07:40 -0600

    transfer balances

--- routes/envelopes.js
+++ routes/envelopes.js
@@ -2,85 +2,128 @@ const express = require("express");
 const router = express.Router();
 
 const {
-    getEnvelopes,
-    getEnvelopesById,
-    addEnvelope,
-    deleteEnvelope,
+  getEnvelopes,
+  addEnvelope,
+  getEnvelopeById,
+  deleteEnvelope,
+	updateEnvelope,
+	transfer,
 } = require("../controllers/envelopes");
 
 /**
  * @swagger
  * /api/v1/envelopes:
- *  get:
+ *    get:
  *      summary: Get all envelopes
  *      produces:
- *          - application/json
+ *        - application/json
  *      tags:
- *          - Envelopes
+ *        - Envelopes
  *      responses:
- *          "200":
- *              description: returns a list of all envelopes
- * 
+ *        "200":
+ *          description: Returns a list of all envelopes
+ *
  */
 router.get("/", getEnvelopes);
 
 /**
  * @swagger
  * /api/v1/envelopes/{id}:
- *  get:
- *      summary: Get an envelopes
+ *    get:
+ *      summary: Get an envelope by ID
  *      produces:
- *          - application/json
+ *        - application/json
  *      tags:
- *          - Envelopes
+ *        - Envelopes
  *      parameters:
- *          -in: path
- *          -name: id
- *          -description: envelope id
- *          -type: integer
- *          -required: true
- *          -example: 1
+ *        - in: path
+ *          name: id
+ *          description: envelope id
+ *          type: integer
+ *          required: true
+ *          example: 1
  *      responses:
- *          "200":
- *              description: Returns an envelope along with its data
- *          "404":
- *              description: User not found
- *          "500":
- *              description: Internal server error
+ *        "200":
+ *          description: Returns a an envelope along with its data
+ *        "404":
+ *          description: User not found
+ *        "500":
+ *          description: Internal server error
  */
-router.get("/:id", getEnvelopesById);
+router.get("/:id", getEnvelopeById);
 
 /**
  * @swagger
  * /api/v1/envelopes:
- *  post:
+ *    post:
  *      summary: Creates a new envelope
  *      produces:
- *          - application/json
+ *        - application/json
  *      tags:
- *          - Envelopes
+ *        - Envelopes
  *      requestBody:
- *          description: Data for new envelope
- *          required: true
- *          content:
- *              application/json:
- *                  schema:
- *                      type: object
- *                      properties:
- *                          title:
- *                              type: string
- *                          budget:
- *                              type: integer
- *                      example:
- *                          title: Scuba lessons
- *                          budget: 300
+ *        description: Data for new envelope
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                title:
+ *                  type: string
+ *                budget:
+ *                  type: integer
+ *              example:
+ *                title: Scuba lessons
+ *                budget: 300
  *      responses:
- *          "201":
- *              description: Returns created envelope
- *          "500":
- *              description: Internal server error
+ *        "201":
+ *          description: Returns created envelope
+ *        "500":
+ *          description: Internal server error
  */
 router.post("/", addEnvelope);
+
+/**
+ * @swagger
+ * /api/v1/envelopes/{id}:
+ *    put:
+ *      summary: Updates an existing envelope
+ *      produces:
+ *        - application/json
+ *      tags:
+ *        - Envelopes
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          description: envelope ID
+ *          type: integer
+ *          required: true
+ *          example: 1
+ *      requestBody:
+ *        description: Data for new envelope
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                title:
+ *                  type: string
+ *                budget:
+ *                  type: integer
+ *              example:
+ *                title: Scuba lessons
+ *                budget: 300
+ *      responses:
+ *        "201":
+ *          description: Returns updated envelope
+ *        "404":
+ *          description: Envelope not found
+ *        "500":
+ *          description: Internal server error
+ */
+router.put("/:id", updateEnvelope);
 
 /**
  * @swagger
@@ -107,5 +150,49 @@ router.post("/", addEnvelope);
  *          description: Envelope not found
  */
 router.delete("/:id", deleteEnvelope);
+
+/**
+ * @swagger
+ * /api/v1/envelopes/{fromId}/transfer/{toId}:
+ *    post:
+ *      summary: Transfers budget from one envelope to another
+ *      produces:
+ *        - application/json
+ *      tags:
+ *        - Envelopes
+*      parameters:
+ *        - in: path
+ *          name: fromId
+ *          description: Origin envelope id
+ *          type: integer
+ *          required: true
+ *          example: 1
+ *        - in: path
+ *          name: toId
+ *          description: Destination envelope id
+ *          type: integer
+ *          required: true
+ *          example: 1
+ *      requestBody:
+ *        description: Amount of money to transfer
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                amount:
+ *                  type: integer
+ *              example:
+ *                amount: 300
+ *      responses:
+ *        "201":
+ *          description: Returns created envelope
+ *        "400":
+ *          description: Bad request
+ *        "500":
+ *          description: Internal server error
+ */
+router.post('/:fromId/transfer/:toId', transfer);
 
 module.exports = router;
